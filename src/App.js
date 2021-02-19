2b500ac339bc8d3b938a000af71ae26f73b68216
human1123 <selliott522@gmail.com>
2021-02-19 16:47:34 -0700

    Fixed a few styling mistakes.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import HomeHeading from "./components/HomeHeading";
+import PastPresFuture from "./components/PastPresFuture";
+import Projects from "./components/Projects";
+import Certs from "./components/Certs";
+import Footer from "./components/Footer";
 
 import "./public/static/css/app.css";
 
-import Home from "./components/Home";
-
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <>
+      <HomeHeading />
 
-        <Route exact path="/about">
-          {/* <About /> */}
-        </Route>
+      <PastPresFuture />
 
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
-    </Router>
+      <Projects />
+
+      <Certs />
+
+      <Footer />
+    </>
   );
 }
+
+// Wireframe: https://wireframe.cc/ROwrUh
 
 export default App;
